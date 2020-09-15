d7ceabefe1258d3a6e462aef207b5df6ea441c21
ryan-fabek-crc <ryan.fabek@canada.ca>
2020-09-15 06:49:27 -0400

    Twitter.py: - Added all RESTAPI functions and confirmed functionality
    index.html:
    - Started implementation of web UI using React

--- Flask_Implementation/Twitter.py
+++ Flask_Implementation/Twitter.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 tweets = [
 
 {
-    'id':1,
+    'id': 1,
     'Author':'Ryan Fabek',
     'Content':'Test'
 
@@ -28,50 +28,93 @@ tweets = [
 
 ]
 
-
-
-
-
 print(data)
 
-@app.route('/tweets', methods=['GET', 'POST','PUT', 'PATCH', 'DELETE'])
-
-def tweet():
 
 
+#App route for retriving all tweets and posting new tweet
+@app.route('/tweets', methods=['GET', 'POST'])
+
+def get_tweet():
+
+    #Retrieves all tweets
     if request.method == 'GET':
 
-        return jsonify({'tweets': tweets})
+        return render_template('index.html')
 
+        #Returns tweets in json format
+        #return jsonify({'tweets': tweets})
 
-    if request.method == 'PUT':
-
-
-        return print('test')
-
-
-    if request.method == 'PATCH':
-
-        return print('test')
-
-
-
+    #Creates a new tweet and adds to json
     if request.method == 'POST':
 
         if not request.json:
             abort(400)
 
-        new_tweet = [{
+        new_tweet = {
             'id': tweets[-1]['id'] + 1,
             'Author': request.json['Author'],
             'Content': request.json.get('Content','')
 
         }
-        ]
+
 
         tweets.append(new_tweet)
 
         return jsonify({'new_tweet' : new_tweet}), 201
+
+
+#App route for editing existing tweets, retriving existing tweet and deleting tweet
+@app.route('/tweets/<int:tweet_id>', methods=['PUT', 'GET', 'DELETE', 'PATCH'])
+
+def get_tweet_2(tweet_id):
+
+
+    if request.method == 'PUT':
+
+        tweet = [tweet for tweet in tweets if tweet['id'] == tweet_id]
+
+        if len(tweet) == 0:
+            abort(404)
+        if not request.json:
+            abort(400)
+
+        tweet[0]['Author'] = request.json.get('Author')
+        tweet[0]['Content'] = request.json.get('Content')
+        return jsonify({'tweet':tweet[0]})
+
+
+    if request.method == 'GET':
+
+        tweet = [tweet for tweet in tweets if tweet['id'] == tweet_id]
+
+        if len(tweet) == 0:
+            abort(404)
+
+        return jsonify({'tweet': tweet[0]})
+
+
+
+
+    if request.method == 'DELETE':
+
+        tweet = [tweet for tweet in tweets if tweet['id'] == tweet_id]
+
+        if len(tweet) == 0:
+            abort(404)
+
+        tweets.remove(tweet[0])
+
+    if request.method == 'PATCH':
+
+        tweet = [tweet for tweet in tweets if tweet['id'] == tweet_id]
+
+        if len(tweet) == 0:
+            abort(404)
+
+        tweet[0]['Author'] = request.json.get('Author', tweet[0]['Author'])
+        tweet[0]['Content'] = request.json.get('Content', tweet[0]['Content'])
+
 
 
 
